6d234f84b9925ba9ccd6e75c5de8d69bf2e1a5b3
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-07-06 23:07:34 +0200

    you can now delete things from the list

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,11 +5,17 @@ class App extends React.Component{
   state={
     todolist:[{id:1, content:"go code"}]
   }
+  deleteTodo = id => {
+    const todos=this.state.todolist.filter(item => item.id !== id)
+    this.setState({
+      todolist:todos
+    })
+  }
   render(){
     return (
       <div className="App">
-        <h1 className="center blue-text"></h1>
-        <ToDo todos={this.state.todolist}/>
+        <h1 className="center blue-text">Todo's</h1>
+        <ToDo todos={this.state.todolist} del={this.deleteTodo}/>
       </div>
 
     );
--- todoapp/src/todos.js
+++ todoapp/src/todos.js
@@ -1,8 +1,8 @@
 import React from "react"
-const ToDo=({todos}) => {
+const ToDo=({todos, del}) => {
     const toDoList=todos.length ? (todos.map(item => {
         return (<div className="collection-item" key={item.id}>
-            <span>{item.content}</span>
+            <span onClick={() => del(item.id)}>{item.content}</span>
         </div>
         )
     })) : (<h1 className="center">You have no todos left yay!</h1>)
