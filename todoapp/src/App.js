d997786930e661e37442e4bbece97b8c04b6499d
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-07-06 21:40:43 +0200

    started project

---
+++ todoapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class App extends React.Component{
+  state={
+    todolist:[{id:1, content:"go code"}]
+  }
+  render(){
+    return (
+      <div className="App">hi</div>
+    );
+  }
+}
+
+export default App;
---
+++ todoapp/src/todos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+class ToDo extends React.Component{
+    render(){
+        return(
+            <div className="todos collection"></div>
+        )
+        }
+}
+export default ToDo;
